c7840d026d3baab7521851fd01a0f054ab89602f
unknown <lim337slaa@hanmail.net>
2021-11-02 21:45:03 +0900

    [Add] : 로그인, 회원가입 페이지 디자인 완료 (기능구현 전)

--- src/Routes.js
+++ src/Routes.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
 import login from './pages/Login/login';
+import signUp from './pages/SignUp/signUp';
+
 export class Routes extends Component {
   render() {
     return (
       <Router>
         <Nav />
         <Switch>
-          <Route exact path="/" component={login} />
+          <Route exact path="/" component={signUp} />
         </Switch>
       </Router>
     );
