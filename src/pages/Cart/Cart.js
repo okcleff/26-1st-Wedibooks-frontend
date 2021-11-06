5c600185feb2c786cf13f44dc2da72c94def4a0b
jyb8509 <bestdung8509@gmail.com>
2021-11-06 16:46:55 +0900

    Cart 화면 MockData 연결

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -3,41 +3,59 @@ import BuyingButton from './BuyingButton/BuyingButton';
 import { AiFillCheckCircle } from 'react-icons/ai';
 import CartBoxSecondLine from './CartBoxSecondLine/CartBoxSecondLine';
 import CartBoxThirdLine from './CartBoxThirdLine/CartBoxThirdLine';
-
+import CartData from './CartBox/CartBox';
 import './Cart.scss';
 
 class Cart extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cartList: [],
+      url: './data/DetailData.json',
+    };
+  }
+
+  componentDidMount() {
+    const { url } = this.state;
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          cartList: data,
+        });
+      });
+  }
+
   render() {
+    const { cartList } = this.state;
     return (
-      <div>
-        <div className="cartContainer">
-          <div className="cartName">카트</div>
-          <div className="cartBoxContainer">
-            <div className="cartBox">
-              <div className="cartBoxFirstLine">구매가능</div>
-              <hr />
-              <CartBoxSecondLine />
-              <hr />
-              <CartBoxThirdLine />
-              <hr />
-              <CartBoxThirdLine />
-              <div className="cartBoxFifthLine" />
+      <div className="cartContainer">
+        <div className="cartName">카트</div>
+        <div className="cartBoxContainer">
+          <div className="cartBox">
+            <div className="cartBoxFirstLine">구매가능</div>
+            <hr />
+            <CartBoxSecondLine />
+            <hr />
+            {cartList.slice(2, 4).map(el => {
+              return <CartBoxThirdLine key={el.id} cartList={el} />;
+            })}
+            <div className="cartBoxFifthLine" />
+          </div>
+          <div className="priceBox">
+            <div className="priceDetails">
+              <ul className="totalPrice">
+                <li className="totalList">
+                  <AiFillCheckCircle className="checkBoxCircle" />
+                  총*권을 선택하셨습니다.
+                </li>
+                <li className="priceList">총 상품 금액</li>
+                <li className="priceSum">
+                  <div className="purchaseTotalPrice">합계</div>
+                </li>
+              </ul>
             </div>
-            <div className="priceBox">
-              <div className="priceDetails">
-                <ul className="totalPrice">
-                  <li className="totalList">
-                    <AiFillCheckCircle className="checkBoxCircle" />
-                    총*권을 선택하셨습니다.
-                  </li>
-                  <li className="priceList">총 상품 금액</li>
-                  <li className="priceSum">
-                    <div className="purchaseTotalPrice">합계</div>
-                  </li>
-                </ul>
-              </div>
-              <BuyingButton title="선택 구매하기" />
-            </div>
+            <BuyingButton title="선택 구매하기" />
           </div>
         </div>
       </div>
--- src/pages/Cart/CartBoxThirdLine/CartBoxThirdLine.js
+++ src/pages/Cart/CartBoxThirdLine/CartBoxThirdLine.js
@@ -4,26 +4,25 @@ import './CartBoxThirdLine.scss';
 
 class CartBoxThirdLine extends Component {
   render() {
+    const { cartList } = this.props;
+    const { imgSrc, name, author, price } = cartList;
+
     return (
       <div className="cartBoxThirdLine">
         <input type="checkbox" className="cb2" />
         <div className="cartBookImage">
-          <img
-            className="book2"
-            alt="book2"
-            src="https://cdn.pixabay.com/photo/2015/06/22/08/39/child-817371__340.jpg"
-          />
+          <img className="checkBook" alt="checkBook" src={imgSrc} />
         </div>
         <div className="bookBoxContents">
           <ul className="bookTitle">
-            <li className="title"> 회복탄력성 </li>
-            <li className="author"> 김주환 저 </li>
+            <li className="title"> {name} </li>
+            <li className="author"> {author} 저 </li>
             <li>
               <DeleteButton title="선택삭제" />
             </li>
           </ul>
         </div>
-        <div className="bookPrice">10,360원</div>
+        <div className="bookPrice">{price}</div>
       </div>
     );
   }
