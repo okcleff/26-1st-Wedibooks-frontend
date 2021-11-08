bb2df0f66322ada9c074a46d0274fcc33fc4621a
jyb8509 <bestdung8509@gmail.com>
2021-11-08 21:26:30 +0900

    mock data 연결

--- src/pages/Details/ReviewsList/ReviewsList.js
+++ src/pages/Details/ReviewsList/ReviewsList.js
@@ -7,20 +7,29 @@ class ReviewsList extends Component {
     const { bookDetail } = this.props;
     return (
       <div className="reviewListBox">
-        <div className="reviewListStarBox" />
-        구매자 리뷰
+        <div className="reviewListStarBox">구매자 리뷰</div>
         <div className="userLine" />
-        {bookDetail.map(el => (
-          <ul className="listParent" key={el.id}>
-            <li className="score">
-              <StarRating />
-              {el.rating}
-            </li>
-            <li className="userID">{el.username}</li>
-            <li className="createDate">{el.created_at}</li>
-            <li className="lastReviewContainer">{el.contents}</li>
-          </ul>
-        ))}
+        <div className="reviewBoxTag">
+          {bookDetail.map(el => (
+            <ul className="listParent" key={el.id}>
+              <li className="score">
+                <StarRating />
+                {el.rating}
+              </li>
+            </ul>
+          ))}
+          {bookDetail.map(el => (
+            <ul className="listChild" key={el.id}>
+              <li className="userID">{el.username}</li>
+              <li className="createDate">{el.created_at.substr(0, 10)}</li>
+            </ul>
+          ))}
+          {bookDetail.map(el => (
+            <ul className="listGrandChild" key={el.id}>
+              <li className="lastReviewContainer">{el.contents}</li>
+            </ul>
+          ))}
+        </div>
       </div>
     );
   }
