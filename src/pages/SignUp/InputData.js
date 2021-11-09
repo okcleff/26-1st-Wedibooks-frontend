7220f065a26c9581dc6577baa8d32fed4e35d5de
unknown <lim337slaa@hanmail.net>
2021-11-09 22:15:31 +0900

    회원가입 페이지 아이디 패스워드 유효성 검사 완료 ( 이메일, 비밀번호 확인 유효성검사 전)

--- src/pages/SignUp/InputData.js
+++ src/pages/SignUp/InputData.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 
 export class InputData extends Component {
-  inputHandle = e => {
-    const { handleInputs } = this.props;
-    handleInputs(e);
+  constructor() {
+    super();
+    this.state = {
+      isValid: false,
+    };
+  }
+  handleInputs = event => {
+    this.props.handleInputs(event);
+
+    if (this.props.name === 'id') {
+      const idReg = /^[A-Za-z]{1}[A-Za-z0-9]{3,}$/;
+      idReg.test(event.target.value)
+        ? this.setState({ isValid: true })
+        : this.setState({ isValid: false });
+    } else if (this.props.name === 'password') {
+      const pwReg = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*-+_=?]).{8,}$/;
+      pwReg.test(event.target.value)
+        ? this.setState({ isValid: true })
+        : this.setState({ isValid: false });
+    }
   };
 
   render() {
-    const { inputType, placeholder, name } = this.props;
+    const { inputType, placeholder, name, maxlength, errormessage } =
+      this.props;
+
     return (
       <div>
         <input
@@ -15,8 +34,11 @@ export class InputData extends Component {
           className="inputData"
           type={inputType}
           placeholder={placeholder}
-          onChange={this.inputHandle}
+          maxlength={maxlength}
+          errormessage={errormessage}
+          onChange={this.handleInputs}
         />
+        {this.state.isValid ? null : <span>{errormessage}</span>}
       </div>
     );
   }
--- src/pages/SignUp/signUp.js
+++ src/pages/SignUp/signUp.js
@@ -17,6 +17,7 @@ export class SignUp extends Component {
       gender: '',
     };
   }
+
   goToMain = e => {
     const { id, password, userName, email, birth, gender } = this.state;
     fetch('http://10.58.5.138:8000/users/signup', {
@@ -60,8 +61,60 @@ export class SignUp extends Component {
     this.setState({
       [e.target.name]: e.target.value,
     });
+
+    // this.isValidID();
+    this.isValidPW();
+    this.isValidPwSame();
+    this.isValidEmail();
   };
 
+  // isValidID = () => {
+  //   const stateId = this.state.id;
+  //   const idReg = /^[A-Za-z]{1}[A-Za-z0-9]{3,}$/;
+  //   const afterTest = idReg.test(stateId);
+
+  //   if (afterTest === false) {
+  //     console.log('id', afterTest);
+  //   } else {
+  //     console.log('id : true', afterTest);
+  //   }
+  // };
+
+  isValidPW = () => {
+    const statePw = this.state.password;
+    const pwReg = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*-+_=?]).{8,}$/;
+    const afterTest = pwReg.test(statePw);
+
+    if (afterTest === false) {
+      console.log('pw', afterTest);
+    } else {
+      console.log('pw : true', afterTest);
+    }
+  };
+
+  isValidPwSame = () => {
+    const statepwCheck = this.state.pwCheck;
+    const statePw = this.state.password;
+
+    if (statepwCheck === statePw) {
+      console.log('same true');
+    } else {
+      console.log('not same false');
+    }
+  };
+
+  isValidEmail = () => {
+    const stateEmail = this.state.email;
+    const emailReg =
+      /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+    const afterTest = emailReg.test(stateEmail);
+
+    if (afterTest === false) {
+      console.log('email : true', afterTest);
+    } else {
+      console.log('email : false', afterTest);
+    }
+  };
   pushMan = () => {
     this.setState({
       gender: '1',
@@ -73,32 +126,14 @@ export class SignUp extends Component {
       gender: '2',
     });
   };
-  // pwInput = e => {
-  //   this.setState({});
-  // };
-
-  // pwCheckInput = e => {
-  //   this.setState({});
-  // };
-
-  // emailInput = e => {
-  //   this.setState({});
-  // };
-
-  // userNameInput = e => {
-  //   this.setState({});
-  // };
-
-  // birthInput = e => {
-  //   this.setState({});
-  // };
-
-  // genderInput = e => {
-  //   this.setState({});
-  // };
 
   render() {
     const { inputList } = this.state;
+    const idReg = /^[A-Za-z]{1}[A-Za-z0-9]{3,}$/;
+    const isVaildID = idReg.test(this.state.id);
+
+    console.log("Hey! I'm " + isVaildID);
+
     return (
       <div className="totalContainer">
         <UserNav />
@@ -111,6 +146,8 @@ export class SignUp extends Component {
                   name={inputList.name}
                   inputType={inputList.inputType}
                   placeholder={inputList.placeholder}
+                  maxlength={inputList.maxLength}
+                  errormessage={inputList.errormessage}
                   handleInputs={this.handleInputs}
                 />
               );
